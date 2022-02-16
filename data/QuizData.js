aa0326f9ad0bbc681b78f544d3fe837a97597ab9
Thameem Ansari <97792702+loyalty-thameem@users.noreply.github.com>
2022-02-16 19:06:12 +0530

    Create QuizData.js

---
+++ data/QuizData.js
@@ -0,0 +1,72 @@
+export default data = [
+  {
+    question:
+      'India is a federal union comprising twenty-eight states and how many union territories?',
+    options: ['6', '7', '8', '9'],
+    correct_option: '8',
+  },
+
+  {
+    question: 'Which of the following is the capital of Arunachal Pradesh?',
+    options: ['Itanagar', 'Dispur', 'Imphal', 'Panaji'],
+    correct_option: 'Itanagar',
+  },
+  {
+    question: 'What are the major languages spoken in Andhra Pradesh?',
+    options: [
+      'Odia and Telugu',
+      'Telugu and Urdu',
+      'Telugu and Kannada',
+      'All of the above languages',
+    ],
+    correct_option: 'Telugu and Urdu',
+  },
+  {
+    question: 'What is the state flower of Haryana?',
+    options: ['Lotus', 'Rhododendron', 'Golden Shower', 'Not declared'],
+    correct_option: 'Lotus',
+  },
+  {
+    question:
+      'Which of the following states is not located in the North?',
+    options: ['Jharkhand','Jammu and Kashmir','Himachal Pradesh','Haryana'
+
+],
+    correct_option: 'Jharkhand',
+  },
+  {
+    question:
+      ' In which of the following state, the main language is Khasi?',
+    options: ['Mizoram','Nagaland','Meghalaya','Tripura'],
+    correct_option: 'Meghalaya',
+  },
+  {
+    question: 'Which is the largest coffee-producing state of India?',
+    options: [
+    'Kerala','Tamil Nadu','Karnataka','Arunachal Pradesh'
+    ],
+    correct_option: 'Karnataka',
+  },
+  {
+    question: 'Which state has the largest area?',
+    options: [
+  'Maharashtra','Madhya Pradesh'
+,'Uttar Pradesh','Rajasthan'
+    ],
+    correct_option: 'Rajasthan',
+  },
+  {
+    question: 'Which state has the largest population?',
+    options: [
+    'Uttar Pradesh','Maharastra','Bihar','Andra Pradesh'
+    ],
+    correct_option: 'Uttar Pradesh',
+  },
+  {
+    question: 'In what state is Elephant Falls located?',
+    options: [
+    'Mizoram','Orissa' ,'Manipur','Meghalaya'
+    ],
+    correct_option: 'Meghalaya',
+  }
+];
